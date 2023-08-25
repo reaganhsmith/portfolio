7af2abad0225c008b2416f76e29c3e664e4c3e4a
reaganhsmith <reagansmith@byui.edu>
2023-08-25 10:07:39 -0700

    changes for color change

--- scripts/index.js
+++ scripts/index.js
@@ -10,21 +10,28 @@ toggleButton.addEventListener("click", () => {
     isButtonClicked = !isButtonClicked;
 
     if (isButtonClicked) {
+        // This is for the new changes we want when the button is clicked
+
         
-        root.style.setProperty('--dark-blue', '#E85FAD'); // Revert to original color
-        root.style.setProperty('--green', '#FCE1C6'); // Revert to original color
-        root.style.setProperty('--dark-pink', '#EE9BDD'); // Revert to original color
-        root.style.setProperty('--light-pink', '#FFEAFB'); // Revert to original color
-        root.style.setProperty('--cream', '#F5F1E7'); // Revert to original color
-        root.style.setProperty('--dark-color', '#311107'); // Revert to original color
+        root.style.setProperty('--dark-blue', '#E85FAD'); // Change to new colors
+        root.style.setProperty('--green', '#FCE1C6'); // Change to new colors
+        root.style.setProperty('--dark-pink', '#EE9BDD'); // Change to new colors
+        root.style.setProperty('--light-pink', '#FFEAFB'); // Change to new colors
+        root.style.setProperty('--cream', '#F5F1E7'); // Change to new colors
+        root.style.setProperty('--dark-color', '#311107'); // Change to new colors
+
         toggleButton.classList.add("button-clicked");
     } else {
-        root.style.setProperty('--dark-blue', '#7BA1A9'); // Change to desired color
-        root.style.setProperty('--green', '#E1E7D0'); // Change to desired text color
-        root.style.setProperty('--dark-pink', '#E2C3C8'); // Change to desired text color
-        root.style.setProperty('--light-pink', '#F4EDEE'); // Change to desired text color
-        root.style.setProperty('--cream', '#F1F0E5'); // Change to desired text color
-        root.style.setProperty('--dark-color', '#193338'); // Change to desired text color
+                // This is for the standard design We want saved 
+
+                
+        root.style.setProperty('--dark-blue', '#7BA1A9'); // Revert to original colorsired color
+        root.style.setProperty('--green', '#E1E7D0'); // Revert to original colorsired text color
+        root.style.setProperty('--dark-pink', '#E2C3C8'); // Revert to original colorsired text color
+        root.style.setProperty('--light-pink', '#F4EDEE'); // Revert to original colorsired text color
+        root.style.setProperty('--cream', '#F1F0E5'); // Revert to original colorsired text color
+        root.style.setProperty('--dark-color', '#193338'); // Revert to original colorsired text color
+
         toggleButton.classList.remove("button-clicked");
     }
 });
